d7b58ee2d743eb752ae207145f38ae44632af2ef
Luis Borja <lbor550@en-331955.uoa.auckland.ac.nz>
2018-04-30 17:34:31 +1200

    Dodgy Detection

--- src/robot_driver/src/laserProcessing.cpp
+++ src/robot_driver/src/laserProcessing.cpp
@@ -14,8 +14,8 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& laserScanData) {
   float count = 0;
   float smallestXInc = 9999;
   float conv = 180/PI;
-
-
+  float prevSlope = 0;
+  int closestElement  = 0;
  //Determine which is the closest point
   for (int i = 0; i < rangeDataNum; ++i) {
     if (laserScanData->ranges[i]< closest) {
@@ -23,6 +23,7 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& laserScanData) {
       closestX = laserScanData->ranges[i] * cos((laserScanData->angle_increment * i)  );
       closestY = laserScanData->ranges[i] * sin((laserScanData->angle_increment * i) );
       angle = i * laserScanData->angle_increment * (180/PI);
+      closestElement = i;
     }
     count = i;
     //ROS_INFO(" [%f] ", i * laserScanData->angle_increment * conv);
@@ -35,7 +36,7 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& laserScanData) {
     float diff = laserScanData->ranges[i] - laserScanData->ranges[i - 1];
     if (diff < -0.05) {
       startPoint = i;
-      ROS_INFO("i: [%f], i -1: [%f]", laserScanData->ranges[i], laserScanData->ranges[i - 1]);
+      //ROS_INFO("i: [%f], i -1: [%f]", laserScanData->ranges[i], laserScanData->ranges[i - 1]);
       break;
     }
 
@@ -53,10 +54,47 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& laserScanData) {
 
 
 
-  ROS_INFO("Cloest Object is at Coordinate: [%f], [%f] at angle [%f] with distance of [%f]", closestX, closestY, angle, closest);
+  int slopeCheck = 0;
+  float startY = laserScanData->ranges[startPoint] * sin((laserScanData->angle_increment * startPoint));
+  float startX = laserScanData->ranges[startPoint] * cos((laserScanData->angle_increment * startPoint));
+  float endY = laserScanData->ranges[endPoint] * sin((laserScanData->angle_increment * endPoint));
+  float endX = laserScanData->ranges[endPoint] * cos((laserScanData->angle_increment * endPoint));
+
+
+
+  float slopeRef = fabs((closestY - startY) / (closestX - startX));
+
+
+  for (int i = startPoint; i <= closestElement; ++i) {
+    float currentY = laserScanData->ranges[i] * sin((laserScanData->angle_increment * i));
+    float currentX = laserScanData->ranges[i] * cos((laserScanData->angle_increment * i));
+    float slope = fabs((currentY - startY) / (currentX - startX));
+
+    if ((slope  <= (slopeRef + (0.10 * slopeRef ))) && (slope  >= (slopeRef - (0.10 * slopeRef )))) {
+      slopeCheck++;
+      ROS_INFO("INSIDE TOLERANCE:, Within [%d]", slopeCheck);
+    }
+    ROS_INFO("Slope ref: [%f], Slope: [%f]", slopeRef, slope);
+
+  }
+
+  if (slopeCheck >= 5) {
+    ROS_INFO("SQUARE");
+  } else {
+    ROS_INFO("CIRCLE");
+  }
+
+  //ROS_INFO("Start X: [%f], closestX: [%f], startY: [%f], closestY: [%f], Slope: [%f]", startX, closestX, startY, closestY, slopeRef);
+
+
+
+
+
+
+  //ROS_INFO("Cloest Object is at Coordinate: [%f], [%f] at angle [%f] with distance of [%f]", closestX, closestY, angle, closest);
   //ROS_INFO("Angle Min = [%f], Angle Max = [%f], Angle Increment = [%f], Count numbers = [%f]", laserScanData->angle_min, laserScanData->angle_max, laserScanData->angle_increment,rangeDataNum);
 
-  ROS_INFO("Edges at [%f], [%f]", startPoint * laserScanData->angle_increment* conv, endPoint * laserScanData->angle_increment * conv);
+  //ROS_INFO("Edges at [%f], [%f]", startPoint * laserScanData->angle_increment* conv, endPoint * laserScanData->angle_increment * conv);
 
 }
 
