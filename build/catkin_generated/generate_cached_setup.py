7cd7aa7cadd094fba93d76d5234a4c730402564c
Luis Borja <lbor550@en-377218.uoa.auckland.ac.nz>
2018-05-03 18:00:28 +1200

    Detection fixed Started Shape

--- build/catkin_generated/generate_cached_setup.py
+++ build/catkin_generated/generate_cached_setup.py
@@ -12,7 +12,7 @@ try:
     from catkin.environment_cache import generate_environment_script
 except ImportError:
     # search for catkin package in all workspaces and prepend to path
-    for workspace in "/opt/ros/kinetic".split(';'):
+    for workspace in "/afs/ec.auckland.ac.nz/users/l/b/lbor550/unixhome/Desktop/GitWorkspace/726_Robot/devel;/opt/ros/kinetic".split(';'):
         python_path = os.path.join(workspace, 'lib/python2.7/dist-packages')
         if os.path.isdir(os.path.join(python_path, 'catkin')):
             sys.path.insert(0, python_path)
--- src/robot_driver/src/laserProcessing.cpp
+++ src/robot_driver/src/laserProcessing.cpp
@@ -5,18 +5,29 @@
 #define PI 3.14159265
 
 
-void checkGradient(int point1, int point2, float &gradient) {
+
+
+void findXY(float length, float angle, float &x, float &y) {
+  x =  length * cos(angle);
+  y = length * sin(angle);
+}
+
+float gradientFinder(float length1, float length2, float angle1, float angle2) {
+  float x1 =0;
+  float y1 = 0;
+  float x2 = 0;
+  float y2 = 0;
+  findXY(length1, angle1, x1, y1);
+  findXY(length2, angle2, x2, y2);
+
+  float gradient = fabs((y2 - y1)/(x2 - x1));
+  ROS_INFO("Gradient: [%f]", gradient);
+  return gradient;
 
 }
 
-void findXY(int point, float &x, float &y) {
-  x = laserScanData->ranges[point] * cos(laserScanData->angle_increment * point);
-  y = laserScanData->ranges[point] * cos(laserScanData->angle_increment * point);
-}
-
-bool within(float value, float compare, float percent)
-{
-  if (value >= (compare - (percent * compare/100)) || value <= (compare + (percent * compare/100))) {
+bool within(float value, float compare, float percent) {
+  if (value >= (compare - (percent * compare/100)) && value <= (compare + (percent * compare/100))) {
     return true;
   } else {
     return false;
@@ -33,22 +44,18 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& laserScanData) {
   float current = 0;
 
  //Determine which is the closest point
- float startPoint = 0;
- float endPoint = rangeDataNum - 1;
- bool startFound = false;
- bool endFound = false;
- float c = 0;
+  float startPoint = 0;
+  float endPoint = rangeDataNum - 1;
+  bool startFound = false;
+  bool endFound = false;
+  float c = 0;
 
   for (int i = 1; i < (rangeDataNum - 1); i++) {
-
     prev1 = laserScanData->ranges[i -1];
     next1 = laserScanData->ranges[i +1];
     current = laserScanData->ranges[i];
-
-
     prevDiff = current - prev1;
     nextDiff = next1 - current;
-
     if (prevDiff <= -0.05 && startFound == false)  {
       startFound = true;
       startPoint = i;
@@ -59,40 +66,55 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& laserScanData) {
   }
   ROS_INFO("\n\n");
 
-  if (startFound == true && endFound == true) {
-    int slopeCheck = 0;
-    float startY = laserScanData->ranges[startPoint] * sin(laserScanData->angle_increment * startPoint);
-    float startX = laserScanData->ranges[startPoint] * cos((laserScanData->angle_increment * startPoint));
-    float endY = laserScanData->ranges[endPoint] * sin((laserScanData->angle_increment * endPoint));
-    float endX = laserScanData->ranges[endPoint] * cos((laserScanData->angle_increment * endPoint));
-    float a = laserScanData->ranges[startPoint];
-    float b = laserScanData->ranges[endPoint];
+  //if (startFound == true && endFound == true) {
+    float startY = 0;
+    float startX = 0;
+    float endY = 0;
+    float endX = 0;
+    float startAngle = laserScanData->angle_increment * startPoint;
+    float endAngle = laserScanData->angle_increment * endPoint;
+    float startLength = laserScanData->ranges[startPoint];
+    float endLength = laserScanData->ranges[endPoint];
+    findXY(startLength, startAngle, startX, startY);
+    findXY(endLength, endAngle, endX, endY);
+    c = sqrt((startLength*startLength) + (endLength*endLength) - (2*startLength*endLength*cos(fabs((startAngle) - (endAngle)))));
 
-
-    c = sqrt((a*a) + (b*b) - (2*a*b*cos(fabs((laserScanData->angle_increment * startPoint) - (laserScanData->angle_increment * endPoint)))));
-/*
-    ROS_INFO("Start X, Start Y: [%f], [%f]", startX, startY);
+    /*ROS_INFO("Start X, Start Y: [%f], [%f]", startX, startY);
     ROS_INFO("End X, End Y: [%f], [%f]", endX, endY);
     ROS_INFO("c: [%f], Angle: [%f]", c, fabs((laserScanData->angle_increment * startPoint) - (laserScanData->angle_increment * endPoint)) * conv);*/
-  }
 
+  //}
   //ROS_INFO("c: [%f]", c);
-  if (c > sqrt(2) || startFound == false || endFound == false || c == 0)
- {
-   ROS_INFO("No OBJECT");
- } else {
-   ROS_INFO("OBJECT");
-   float midPoint = fabs((startPoint + endPoint) / 2)
+    if (c > sqrt(2) || startFound == false || endFound == false || c == 0)
+    {
+     ROS_INFO("No OBJECT");
+    } else {
+     ROS_INFO("OBJECT");
+     int startCheck = 0;
+     int endCheck = 0;
+     float midPoint = fabs((startPoint + endPoint) / 2);
+     float midAngle = laserScanData->angle_increment * midPoint;
+     float midLength = laserScanData->ranges[midPoint];
+     float slopeRefStart = gradientFinder(midLength, startLength, startAngle, midAngle);
+     float slopeRefEnd = gradientFinder(midLength, endLength,endAngle,  midAngle);
+     for (int i = startPoint; i <endPoint; i++) {
+       if (within(gradientFinder(laserScanData->ranges[i], startLength, laserScanData->angle_increment * i, startAngle), slopeRefStart, 5)) {
+          startCheck++;
+       } else if (within(gradientFinder(laserScanData->ranges[i], endLength, laserScanData->angle_increment * i, endAngle), slopeRefEnd, 5)) {
+         endCheck++;
+       }
+     }
+     ROS_INFO("Start Count [%d]", startCheck );
+     ROS_INFO("End Count [%d]", endCheck );
+     if (endCheck > 5 || startCheck > 5) {
+       ROS_INFO("SQUARE");
+     } else {
+       ROS_INFO("CIRCLE");
+     }
 
+
+    }
   }
-
-
- }
-
-
-  //ROS_INFO("Cloest Object is at Coordinate: [%f], [%f] at angle [%f] with distance of [%f]", closestX, closestY, angle, closest);
-  //ROS_INFO("Angle Min = [%f], Angle Max = [%f], Angle Increment = [%f], Count numbers = [%f]", laserScanData->angle_min, laserScanData->angle_max, laserScanData->angle_increment,rangeDataNum);
-
 
 
 int main(int argc, char **argv)
